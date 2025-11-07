22f68dd04504dcd7a97738d20c9450223aa53cfc
linaarisoy21-oss <lina.arisoy21@gmail.com>
2025-11-07 13:37:34 +0400

    Add files via upload

---
+++ L02 Understanding The Variables.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x= 5
+y = "Lina"
+
+print(x)
+print(y)
+
+# User Input
+name = input("enter your name: ")
+
+print()
+print("Hello " + name)
+print("welcome to Codingal")

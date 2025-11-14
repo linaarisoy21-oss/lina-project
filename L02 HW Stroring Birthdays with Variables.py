0ae5d93a1b0a62766c6381d08d4228778f31d0a1
linaarisoy21-oss <lina.arisoy21@gmail.com>
2025-11-14 14:59:49 +0400

    Add files via upload

---
+++ L02 HW Stroring Birthdays with Variables.py
@@ -0,0 +1,23 @@
+# 5 friends and their birthdays
+
+friend1_name = "Aliya"
+friend1_birthday = "12/05/2010"
+
+friend2_name = "Mira"
+friend2_birthday = "20/11/2013"
+
+friend3_name = "Judy"
+friend3_birthday = "01/09/2011"
+
+friend4_name = "Fatma"
+friend4_birthday = "23/08/2012"
+
+friend5_name = "Veronika"
+friend5_birthday = "09/03/2009"
+
+# Printing the Birthdays
+print(friend1_name, "has a birthday on", friend1_birthday)
+print(friend2_name, "has a birthday on", friend2_birthday)
+print(friend3_name, "has a birthday on", friend3_birthday)
+print(friend4_name, "has a birthday on", friend4_birthday)
+print(friend5_name, "has a birthday on", friend5_birthday)

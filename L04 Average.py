c7649f2ecbf7a5a69cdbecc8b4188cd7e7c7d744
linaarisoy21-oss <lina.arisoy21@gmail.com>
2025-11-21 13:40:29 +0400

    Add files via upload

---
+++ L04 Average.py
@@ -0,0 +1,15 @@
+# Storing values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+# Finding the total number of trees
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("the sum of all the 5 treees is: ", sum)
+
+ # Finding the average of the trees
+average = sum / 5
+
+print("the average of all the trees is :", average)

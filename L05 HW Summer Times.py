3d90743eb8a6ec5dce108648a5ecc9fb4618a31a
linaarisoy21-oss <lina.arisoy21@gmail.com>
2025-11-28 15:12:17 +0400

    Add files via upload

---
+++ L05 HW Summer Times.py
@@ -0,0 +1,8 @@
+temp = int(input("enter the temperature (in celcius): "))
+
+if temp < 25:
+    print("it's cold, wear jacket or pullover")
+elif temp >50:
+    print("that's not normal! it's burning hot")
+else:
+    print("it's hot during the summer, wear light and soft clothes")

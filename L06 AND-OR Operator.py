3374b58d5c9859e824301f6228503febc9f31a2a
linaarisoy21-oss <lina.arisoy21@gmail.com>
2025-12-05 13:37:08 +0400

    Add files via upload

---
+++ L06 AND-OR Operator.py
@@ -0,0 +1,23 @@
+a = 10
+b = 12
+c = 0 #It means false
+
+if a and b and c:
+  print("All the numbers have boolean value as true")
+else:
+   print("At least one number has a boolean value as false")
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either one of the number is greater than 0")
+else:
+   print("No number is greater than 0")
+
+if b > 0 or c > 0:
+   print ("Either 0f the number is greater than 0")
+else:
+   print("No number is greater than 0")
+

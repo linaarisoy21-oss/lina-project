aeecc8c7d7a4e4ff4e56e16521e3bcf2fef75fff
linaarisoy21-oss <lina.arisoy21@gmail.com>
2025-12-12 13:39:55 +0400

    Add files via upload

---
+++ L07 Bitwise Operator.py
@@ -0,0 +1,18 @@
+a = 10
+b = -10
+
+# print bitwise right shift operator
+print("after right shift")
+print("a >> 1 =",a >> 1)
+print("b >> 1 =", b >> 1)
+
+# pressing enter 2 times
+print()
+
+a = 5
+b = -10
+
+# print bitwise left shift operator
+print("after left shift")
+print("a << 1 =",a << 1)
+print("b << 1 =", b << 1)

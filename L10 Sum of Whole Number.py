e089e4dedbe2d74a729750318613fe28fa26c96a
linaarisoy21-oss <lina.arisoy21@gmail.com>
2026-01-16 13:36:10 +0400

    Add files via upload

--- L10 Sum of Whole Number.py
+++ L10 Sum of Whole Number.py
@@ -2,7 +2,7 @@ n = int(input("enter the number whose sum you want to find: "))
 
 sum = 0
 
-for i in range (1, n+1):
+for i in range(1, n+1):
     sum = sum + i
     print(f"i = {i}")
     print(f"Sum of {i} = {sum}")

6319f3d54c6448281e2ce3a33dc81d85a724a0eb
linaarisoy21-oss <lina.arisoy21@gmail.com>
2026-03-13 13:44:10 +0400

    Add files via upload

---
+++ L15 Weather Condition.py
@@ -0,0 +1,9 @@
+def weather_condition(): # fuction declaration
+    print("weather is pleasant in",x)
+    print("weather is the same in",y)
+
+# x and y decleration must be before triggering/calling the function otherwise it gives an error
+x="autumn"
+y="winter"
+
+weather_condition() # call the function
